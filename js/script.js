74f60851c78b33802420196b9e0066bbe20df014
Kateryna Bilinska <katriiin132@gmail.com>
2022-01-13 21:51:53 +0200

    Adding slider to the index.html

--- js/script.js
+++ js/script.js
@@ -13,3 +13,38 @@ $('.mouse-paralax').on('mousemove', (e) => {
     );
 
 });
+
+function nextSlide() {showSlides(slideIndex += 1);}
+
+function previousSlide() {showSlides(slideIndex -= 1);}
+
+function currentSlide(n) {showSlides(slideIndex = n);}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slider-item");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (let slide of slides) {slide.style.display = "none";}
+
+    slides[slideIndex - 1].style.display = "block";    
+}
+
+let slideIndex = 0;
+
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slider-item");
+
+  for (i = 0; i < x.length; i++) { x[i].style.display = "none";}
+
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+
+  x[slideIndex-1].style.display = "block";
+  
+  setTimeout(carousel, 5000);
+}
